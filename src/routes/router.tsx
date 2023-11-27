02bafd554f22fb0095094cff3983e06a2c229ada
Tanmay Mishra <tanmayarya29@gmail.com>
2023-11-27 10:53:32 +0000

    initial setup

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import NullPage from "../pages/NullPage";
 import OfferListPage from "../pages/OfferListPage";
-import OfferAddPage from "../pages/OfferAddPage";
 import LoginPage from "../pages/LoginPage";
 
 export const router = createBrowserRouter([
@@ -10,46 +9,8 @@ export const router = createBrowserRouter([
     element: <OfferListPage />,
   },
   {
-    path: "/add",
-    element: <OfferAddPage />,
-  },
-  {
-    path: "/edit",
-    element: <OfferAddPage />,
-  },
-  {
     path: "/login",
     element: <LoginPage />,
-  },
-  {
-    // path: "/",
-    // element: <Layout />,
-    // children: [
-    //   {
-    //     path: "/",
-    //     element: <ProductListingPage />,
-    //   },
-    //   {
-    //     path: "sign-up",
-    //     element: <RegistrationPage />,
-    //   },
-    //   {
-    //     path: "sign-in",
-    //     element: <LoginPage />,
-    //   },
-    //   {
-    //     path: "add-product",
-    //     element: <ProductAddOrEditPage />,
-    //   },
-    //   {
-    //     path: "/edit-product/:id",
-    //     element: <ProductAddOrEditPage />,
-    //   },
-    //   {
-    //     path: "*",
-    //     element: <NullPage />,
-    //   },
-    // ],
   },
   {
     path: "*",
